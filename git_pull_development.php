2b072269350fc32827f8819fa737c06d5f633756
benedictnkosi <nkosi.benedict@gmail.com>
2022-07-25 15:14:48 +0200

    WIP

---
+++ git_pull_development.php
@@ -0,0 +1,30 @@
+<?php
+$command = "git pull origin development --force";
+//$command = "git clone https://github.com/benedictnkosi/aluve_front_end.git --branch main";
+
+exec($command.' 2>&1', $tmp, $return_code); // Execute the command
+
+// Output the result
+
+printf('
+    
+<span class="prompt">$</span> <span class="command">%s</span>
+    
+<div class="output">%s</div>
+    
+'
+    
+    , htmlentities(trim($command))
+    
+    , htmlentities(trim(implode("\n", $tmp)))
+    
+    );
+
+$output = ob_get_contents();
+
+echo $tmp;
+
+echo $output;
+
+?>
+
