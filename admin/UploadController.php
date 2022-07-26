ad69c688bdf8073fecfb51068290122b1b703ad1
benedictnkosi <nkosi.benedict@gmail.com>
2022-07-26 11:54:48 +0200

    WIP

--- admin/UploadController.php
+++ admin/UploadController.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/application.php');
 $uploader   =   new Uploader();
 $uploader->setDir(__DIR__ . '/../assets/images/rooms/');
 $uploader->setExtensions(array('jpg','jpeg','png','gif'));  //allowed extensions list//
-$uploader->setMaxSize(.5);                          //set max file size to be allowed in MB//
+$uploader->setMaxSize(5);                          //set max file size to be allowed in MB//
 
 if($uploader->uploadFile('files')){   //txtFile is the filebrowse element name //
     $image  =   $uploader->getUploadName(); //get uploaded file name, renames on upload//
